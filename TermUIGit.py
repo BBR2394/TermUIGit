bc6c9b1a6975c853816077de1037acd1640a0e30
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2019-01-25 14:26:10 +0100

    start working to manage the repo, so create functions and preparing corectly the json file, but it looks like i dont have git lib for python ...

--- TermUIGit.py
+++ TermUIGit.py
@@ -7,6 +7,7 @@ import curses
 from time import sleep
 import json
 
+langFileName = "langFile.json"
 langFIle = None
 repoFile = None
 langJson = None
@@ -37,35 +38,46 @@ def select_language(tabLang):
 
 	if langStorage == None:
 		raise Exception('language', 'there is no language selected. no default could be selected')
-	print("fin de lang choisi")
+	#print("fin de lang choisi")
 	print(langStorage['test'])
 
 
 def open_lang_file():
 	try:
-		langFile = open('langMsg-HumanInterfaceDownloarderGitRepo.json', 'r')
-		print("le fichier langue est ouvert")
+		langFile = open(langFileName, 'r')
+		#print("lang file opened")
 		data = langFile.read()
 		print(data)
 	except Exception as inst:
-		print(type(inst))
-		print(inst.args)
-		print(inst)
-		print('an error occured when openfing the file')
+		#print(type(inst))
+		#print(inst.args)
+		#print(inst)
+		print('-> an error occured when openfing the file', sys.stderr)
 		return False
 	try:
 		langJson = json.loads(data)
-		print(langJson)
+		#print(langJson)
 	except Exception as inst:
-		print('probleme with json file lang')
+		print('-> probleme with json file lang', sys.stderr)
 
-	print("jessaye de voir pour afficher le json")
-	print(langJson['Languages'])
-	print(langJson['Languages'][0]['code'])
-	print(langJson['Languages'][1]['code'])
+	#print("jessaye de voir pour afficher le json")
+	#print(langJson['Languages'])
+	#print(langJson['Languages'][0]['code'])
+	#print(langJson['Languages'][1]['code'])
 	select_language(langJson['Languages'])
 
+def print_repo_file():
+	print("=>dans print repo file")
+	print(repoJson)
+	print(repoJson['repo'])
+	print(repoJson['repo']['epitech'])
+	print(repoJson['repo']['github'])
+	print(repoJson['repo']['epitech']['tek1'])
+	print(repoJson['repo']['epitech']['tek2'])
+	print(repoJson['repo']['epitech']['tek2']['piscineCpp'])
+
 def open_repo_file():
+	global repoJson
 	try:
 		repoFile = open('repoList.json', 'r')
 		print("le fichier est ouvert")
@@ -79,11 +91,15 @@ def open_repo_file():
 		return False
 	try:
 		repoJson = json.loads(data)
-		print(repoJson)
 	except Exception as inst:
-		print('probleme with json file repo')
+		print('-> probleme with json file repo', sys.stderr)
+	print_repo_file()
 
-
+def clone_repo():
+	global repoJson
+	print("sans la fonctio clone repo")
+	print(repoJson['repo']['github']['tous'])
+	
 
 def stop_curse_window(cuvar):
 	curses.nocbreak()
@@ -112,8 +128,9 @@ print("* démarrage dans *")
 
 print(sys.argv)
 open_lang_file()
-#open_repo_file()
-curseVar = None
-curseVar = start_curse_window(curseVar)
-stop_curse_window(curseVar)
+open_repo_file()
+clone_repo()
+#curseVar = None
+#curseVar = start_curse_window(curseVar)
+#stop_curse_window(curseVar)
 
